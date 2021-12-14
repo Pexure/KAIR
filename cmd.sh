c0a23e85fb7c63c9a051db5e130879ac05bdb47e
pexure <pexure@gmail.com>
2021-12-14 21:41:59 +0800

    m

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,9 @@
 # DDP train
 python -m torch.distributed.launch --nproc_per_node=8 --master_port=1234 main_train_psnr.py \
-  --opt options/swinir/train_swinir_sr_classical.json  --dist True
+  --opt options/swinir/train_swinir_sr_classical_x2.json  --dist True
 
 # DP train
-python main_train_psnr.py --opt options/swinir/train_swinir_sr_classical.json
+python main_train_psnr.py --opt options/swinir/train_swinir_sr_classical_x2.json
 
 # test model_zoo
 python main_test_swinir.py --task classical_sr --scale 2 --training_patch_size 48 \
